84679c90aa0893f932504eb857652c52eb4ebcb2
danila-derenchenko <79960110+danila-derenchenko@users.noreply.github.com>
2022-12-15 21:08:09 +0400

    second lesson

--- main.py
+++ main.py
@@ -1,65 +1,54 @@
-import math
+'''print("Program 1")
 
-print("Program 1")
+number = input("Input number: ")
+lenght = len(number)
+number = int(number)
+result = 0
 
-day = int(input("Введите номер дня недели: "))
+for i in range(1, lenght + 1):
+    result += number % 10
+    number //= 10
 
-if day == 6 or day == 7:
-    print("Это выходной")
-else:
-    print("Это не выходной")
+print(result)
 
 print("Program 2")
 
-flag = 0
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not(x or y or z) == (not(x) and not(y) and not (z)):
-                flag = 1
-            else:
-                flag = 0
-if flag == 1:
-    print("Выражение истинно")
-else:
-    print("Выражение ложно")
+n = int(input("Input n: "))
+
+pred = 1
+result = []
+
+for i in range(1, n + 1):
+    pred *= i
+    result.append(pred)
+for i in result:
+    print(i)
 
 print("Program 3")
 
-x = int(input("Введите x: "))
-y = int(input("Введите y: "))
+n = int(input("Input n: "))
 
-if x > 0 and y > 0:
-    print("Это первая четверть")
-elif x < 0 and y > 0:
-    print("Это вторая четверть")
-elif x < 0 and y < 0:
-    print("Это третья четверть")
-elif x > 0 and y < 0:
-    print("Это четвёртая четверть")
-else:
-    print("x и y не должны быть равны нулю")
+result = [2]
+
+number = 2
+
+for i in range(n):
+    number = (1 + (1 / number)) ** number
+    result.append(number)
+
+print(sum(result))'''
 
 print("Program 4")
 
-a = int(input("Введите номер четверти: "))
-
-if a == 1:
-    print("И x и y должны быть положительными")
-elif a == 2:
-    print("x отрицательное, y положительное")
-elif a == 3:
-    print("И x, и y отрицательные")
-elif a == 4:
-    print("x положительное, y отрицательное")
-else:
-    print("Введён неверный номер четверти")
-
-print("Program 5")
-
-x1 = int(input("Введите x1: "))
-y1 = int(input("Введите y1: "))
-x2 = int(input("Введите x2: "))
-y2 = int(input("Введите y2: "))
-
-print("Расстояние между двумя введёнными точками: " + str(math.dist((x1, y1), (x2, y2))))
+with open("test.txt") as f:
+    positionsText = f.readlines()
+    positions = [int(x) for x in positionsText]
+    n = int(input("Input n: "))
+    prom = []
+    for i in range(-n, n + 1):
+        prom.append(i)
+    result = prom[positions[0]]
+    positions.remove(positions[0])
+    for i in positions:
+        result *= prom[i]
+    print(result)
